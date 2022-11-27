4ab7476688abfa95d2432847ee22ccef8cb9cbfa
Jokieh <1stone.seres@gmail.com>
2022-11-27 12:42:20 +0100

    finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(input, times) {
 
+    let value = "";
+
+    for (let i=0 ; i < times; i++) {
+
+        value += input;
+
+    }
+
+    if (times < 0) return "ERROR";
+
+    return value;
 };
 
 // Do not edit below this line
