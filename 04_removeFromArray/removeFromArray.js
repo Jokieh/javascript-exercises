aaf36cc37105c3ce500b3b3168576b5e3da2c94d
Jokieh <1stone.seres@gmail.com>
2022-11-29 16:34:18 +0100

    finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+
+    let i,j;
+
+    for (i = (array.length-1); i >= 0; i--) {
+
+        for (j=0; j < args.length; j++) {
+
+            if (args[j] === array[i]) {
+
+                array.splice(i,1);
+
+            }
+        }
+    }
+
+    return array;
 
 };
 
