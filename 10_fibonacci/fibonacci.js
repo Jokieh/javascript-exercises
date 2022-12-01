48f697ec98028fcea4c46e7677fecff060ad38fc
Jokieh <1stone.seres@gmail.com>
2022-12-01 07:20:04 +0100

    finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+    if (number < 0) return "OOPS";
+
+    number = parseInt(number);
+
+    let fibonacci = [1,1];
+
+    if (typeof number == "string") {
+        number = parseInt(number);
+    }
+
+    if (number >= 3) {
+        for (let i = 2; i <= number-1; i++) {
+            fibonacci.push(fibonacci[fibonacci.length-2] + fibonacci[fibonacci.length-1]);
+        }
+    }
+
+    return fibonacci[fibonacci.length-1];
 
 };
 
