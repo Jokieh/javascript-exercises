184a8aa305de1104f3f35784ee1c3ccb657ec745
Jokieh <1stone.seres@gmail.com>
2022-12-01 09:24:29 +0100

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(inputArray) {
 
-};
+    let oldestAge = 0;
+    let oldestName = "";
+    let date = parseInt(new Date().getFullYear().toString());
+
+    inputArray.forEach(member => {
+
+        let yearOfDeath = 0;
+
+        if (typeof member.yearOfDeath === 'undefined') {
+            yearOfDeath = date;
+        } else {
+            yearOfDeath = member.yearOfDeath;
+        }
+
+
+       if ((yearOfDeath - member.yearOfBirth) > oldestAge) {
+           oldestAge = yearOfDeath - member.yearOfBirth;
+           oldestName = member.name;
+       }
+    })
+
+    return inputArray.find(element => element.name == oldestName);
+
+}
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
