3057f5d5dd73c6fba7ffcbe15e438848cfd7d68b
Jokieh <1stone.seres@gmail.com>
2022-12-01 06:58:56 +0100

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (input) {
+
+    input = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase();
+
+    let isPalindrome = false;
+
+    let j = input.length-1;
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] == input[j]) {
+                isPalindrome = true;
+            } else {
+                isPalindrome = false;
+                break;
+            }
+        j--;
+    }
+
+    return isPalindrome;
 
 };
 
